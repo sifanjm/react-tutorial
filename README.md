f2ebe2af7439baf1022a4a6a4356c3934b410f91
sifanjm <sifanjm@gmail.com>
2021-01-29 14:40:38 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-6lvuo2
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-6lvuo2)
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+ReactDOM.render(
+  <h1>Hello, world!</h1>,
+ document.getElementById("root")
+);
+
