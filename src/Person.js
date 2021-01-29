c5afa4c8717f47ea272a84c12b28cfd5d138b4cb
sifanjm <sifanjm@gmail.com>
2021-01-29 15:02:15 +0530

    statefulcomponent

---
+++ src/Person.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default class Person extends React.Component {
+  render() {
+    return (
+      <>
+      <p>Hello, world!</p>
+      <h3>Hi</h3>
+    </>
+    );
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from "react";
+import ReactDOM from "react-dom";
+import Person from "./Person"
 
 ReactDOM.render(
-  <h1>Hello, world!</h1>,
- document.getElementById("root")
+  <Person/>,
+  document.getElementById('root')
 );
-
